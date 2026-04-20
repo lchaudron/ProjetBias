dda459968e720649c714a18dd16d6a8c13ab38d8
lchaudron <lucien.g.chaudron@gmail.com>
2026-04-20 14:32:56 +0200

    changed folder name

--- test.py
+++ test.py
@@ -1,44 +1,13 @@
-import pandas as pd
-import joblib
-import numpy as np
+from src.preprocessing.preprocessor import Preprocessor
 
-# 1. Charger les pipelines (exemple pour le Random Forest à chaque étape)
-phases = ['Inscription', '1st_Sem', '2nd_Sem']
-model_types = ['gradient_boosting', 'logistic_regression', 'mlp', 'random_forest']
+preprocessor = Preprocessor()
 
-# Génération automatique du dictionnaire des chemins
-model_paths = {
-    phase: {
-        model: f"outputs/models/{phase}/{model}_best_model.pkl"
-        for model in model_types
-    }
-    for phase in phases
-}
 
-def predire_etudiant(data_dict, phase, model_type):
-    # Transformer le dictionnaire en DataFrame d'une seule ligne
-    X_new = pd.DataFrame([data_dict])
-    
-    # Sélectionner le bon pipeline
-    pipe = joblib.load(model_paths[phase][model_type])
-    
-    # Obtenir la prédiction et les probabilités
-    classe_predite = pipe.predict(X_new)[0]
-    probabilites = pipe.predict_proba(X_new)[0]
-    confiance = np.max(probabilites)
-    
-    # Mapping des labels (à adapter selon votre LabelEncoder)
-    mapping = {0: 'No dropout', 1: 'Dropout'}
-    
-    return mapping[classe_predite], confiance, probabilites
+path = "data/mean_student/gend-nat/Gender-0_Nacionality-Africa.csv"
+path_ref = "data/final/data_2nd_sem.csv"
 
-df = pd.read_csv('data/mean_student/mean_student_encoded.csv', sep=';')
+df_inscription, df_1st_sem, df_2nd_sem = preprocessor.process_and_save(path, path_ref)
 
-# Exemple d'utilisation
-data_dict = df.iloc[0].to_dict()  # Convertir la première ligne en dictionnaire
-phase = '2nd_Sem'
 
-for model_type in model_types:
-    prediction, confidence, probabilities = predire_etudiant(data_dict, phase, model_type)
-    print(f"Modèle: {model_type}")
-    print(f"Prédiction: {prediction}, Confiance: {confidence:.2f}, Probabilités: {probabilities}\n")
+
+
